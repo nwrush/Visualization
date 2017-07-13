a0ea9dc1b8c8b2ac71837c67f02a8fbca7bc2343
Nikko Rush <rushni@uw.edu>
2017-07-13 12:13:31 -0700

    Run the preprocessor as a subprocess

--- Visualizer/startup.py
+++ Visualizer/startup.py
@@ -5,50 +5,57 @@
 Run the preprocessor (ask for arguments, etc.) and feed the output into the gui
 """
 
-def main():
-    import datetime
-    print(datetime.datetime.now())
-    import os
-    import os.path
-    import sys
+import datetime
+print(datetime.datetime.now())
+import os
+import os.path
+import subprocess
+import sys
 
-    # Import matplotlib to ensure the backend it set to tk
-    import matplotlib
-    import tkinter
-    matplotlib.use("TKAgg")
 
-    from data_processor import main
-    import Visualizer
-    class Namespace:
-        #def __getattr__(self, name):
-        #    return None
-        pass
+# Import matplotlib to ensure the backend it set to tk
+import matplotlib
+matplotlib.use("TKAgg")
 
-    def read_config_file(fname):
-        if not os.path.isfile(fname):
-            print("Error: Couldn't find file {0}".format(fname))
-            sys.exit(1)
+#from data_processor import main
+import Visualizer
 
-        f = open(fname, 'r')
+class Namespace:
+    #def __getattr__(self, name):
+    #    return None
+    pass
 
-        args = []
-        for line in f.readlines():
-            line = line.strip()
-            if line == '':
-                continue
+def read_config_file(fname):
+    if not os.path.isfile(fname):
+        print("Error: Couldn't find file {0}".format(fname))
+        sys.exit(1)
 
-            name, value = line.split('=')
+    f = open(fname, 'r')
 
-            args.append("--{0}".format(name))
-            args.append(value)
-        
-        return args
+    args = []
+    for line in f.readlines():
+        line = line.strip()
+        if line == '':
+            continue
 
-    print(datetime.datetime.now())
-    args = read_config_file("Keywords.config")
+        name, value = line.split('=')
 
-    print(datetime.datetime.now())
-    #args = main.main(args, True)
+        args.append("--{0}".format(name))
+        args.append(value)
 
-    print(datetime.datetime.now())
-    Visualizer.main("keywords_data.p")
+    return args
+
+def run_preprocessor(args):
+    path = ["python", "main.py"] + args
+    cwd = "../idea_relations/data_processor"
+    subprocess.run(path, cwd=cwd)
+
+print(datetime.datetime.now())
+args = read_config_file("Keywords.config")
+
+print(datetime.datetime.now())
+run_preprocessor(args)
+#args = main.main(args, True)
+
+print(datetime.datetime.now())
+Visualizer.main("keywords_data.p")
