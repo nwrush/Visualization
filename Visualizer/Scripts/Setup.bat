5c08c49d039cc78b411a9484c6367ba0a4eb27ed
Nikko Rush <rushni@uw.edu>
2017-07-21 13:15:22 -0700

    Fixed setup script

--- Visualizer/Scripts/Setup.bat
+++ Visualizer/Scripts/Setup.bat
@@ -3,6 +3,6 @@
 cd ..
 
 virtualenv --python=C:\Users\rushni\AppData\Local\Programs\Python\Python35\python.exe venv
-.\venv\Scripts\activate.bat
+call .\venv\Scripts\activate.bat
 
 pip install -r requirements.txt
